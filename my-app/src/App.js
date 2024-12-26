dc4785a99bfac24d41164d7a485d5e5eec56510d
ysilksa <isabella.yan.business@gmail.com>
2024-12-25 18:19:43 -0600

    Discover.js readability

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,23 +9,6 @@ import "./NavBarStyles.css"
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-  // to create light and dark mode 
-  // const [mode, setMode] = useState('light');
-
-  // const switchMode = () => {
-  //   if (mode === 'light') {
-  //     setMode('dark');
-  //   } else if (mode === 'dark') {
-  //     setMode('light');
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   console.log(mode);
-  // }, [mode]);
-
-  // const backgroundColor = mode === 'light' ? '#FFFDF0' : '#2A2B2A';
-  // const textColor = mode === 'light' ? '#2A2B2A' : '#FFFDF0' ;
 
   return (
     <>
